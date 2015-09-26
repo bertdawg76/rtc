3e8860e51a6590e7940c9b6025525ac2c66dc448
Bert Coleman <bertcoleman@Berts-MBP-2.lan>
2015-09-25 23:20:21 -0600

    reformatting the file.

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express'),
-	db = require('./config/database.js'),
+    db = require('./config/database.js'),
     config = require('./config/passport.js'),
     bodyParser = require('body-parser'),
     cors = require('cors'),
-   
+
     passport = require('passport'),
     mongoose = require('mongoose'),
     server = express(),
@@ -11,22 +11,16 @@ var express = require('express'),
     questionCtrl = require('./questions/questionCtrl.js')(server, express, router),
     userCtrl = require('./users/userCtrl.js')(server, express, router),
     port = process.env.PORT || 9000;
-    //mongoUri = 'mongodb://bert:adminpassword@ds051943.mongolab.com:51943/rtc-practice',
-    
- 	/*questionCtrl = require('./questions/questionCtrl'),*/
- 	
- 	
+//mongoUri = 'mongodb://bert:adminpassword@ds051943.mongolab.com:51943/rtc-practice',
+
+/*questionCtrl = require('./questions/questionCtrl'),*/
+
+
 db.connect();
- 	
-
-
-
-
-
 
 
 server.use(cors());
-server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.urlencoded({extended: true}));
 server.use(bodyParser.json());
 
 
@@ -43,16 +37,13 @@ server.use('/', router);
 //});
 
 
-
 /*server.use(function(req, res, next) {
-	var err = new Error('not found');
-	err.status = 404;
-	next(err);
-})*/
+ var err = new Error('not found');
+ err.status = 404;
+ next(err);
+ })*/
 
 
-
-
-server.listen(port, function() {
+server.listen(port, function () {
   console.log('connected to: ', port);
 });
